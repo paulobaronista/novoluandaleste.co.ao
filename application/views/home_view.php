ccaf08cea45c2083ef98648924d5aa215b64151d
PauloBaronista <paulo@rpm.com.br>
2021-11-20 02:31:10 -0300

    Ajuste layout

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -9,7 +9,9 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <a class="navbar-brand scrollink" href="#assine"><img src="<?= base_url(); ?>assets/images/logo.png" alt="Novo Luanda Leste"></a>
+                    <a class="navbar-brand scrollink" href="#home">
+                        <img class="hidden-xs" src="<?= base_url(); ?>assets/images/logo.png" alt="Novo Luanda Leste">
+                    </a>
                 </div>
                 <div id="navbar" class="navbar-collapse collapse">
                     <div class="redes">
@@ -29,68 +31,119 @@
         </div>
     </div>
 </nav>
-<div id="projecto" class="container-fluid padding-off projecto">
+<div id="home" class="container-fluid padding-off home">
+    <div class="galeria">
+        <section class="glr-home slider">
+            <div>
+                <img class="img-responsive center-block hidden-xs" src="<?= base_url(); ?>assets/images/Galeria_01.jpg" alt="Novo Luanda Leste">
+                <img class="img-responsive center-block visible-xs" src="<?= base_url(); ?>assets/images/Galeria_mobile_01.jpg" alt="Novo Luanda Leste">
+            </div>
+            <div>
+                <img class="img-responsive center-block hidden-xs" src="<?= base_url(); ?>assets/images/Galeria_02.jpg" alt="Novo Luanda Leste">
+                <img class="img-responsive center-block visible-xs" src="<?= base_url(); ?>assets/images/Galeria_mobile_02.jpg" alt="Novo Luanda Leste">
+            </div>
+            <div>
+                <img class="img-responsive center-block hidden-xs" src="<?= base_url(); ?>assets/images/Galeria_03.jpg" alt="Novo Luanda Leste">
+                <img class="img-responsive center-block visible-xs" src="<?= base_url(); ?>assets/images/Galeria_mobile_03.jpg" alt="Novo Luanda Leste">
+            </div>
+        </section>
+    </div>
+</div>
+
+<!-- 
+<div class="col-xs-12 col-sm-5 col-md-5 col-lg-5 padding-off">
+    <form method="post" role="form" action="<?php echo base_url("contato") ?>">
+        <div class="group-form">
+            <div class="col-xs-12 col-sm-12 col-md-12 padding-off contato">
+                <div class="col-xs-12 col-sm-12 col-md-12 form-group">
+                    <h5>Quer receber mais informações?</h5>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12 form-group">
+                    <input id="nome" type="text" class="nome form-control" name="nome" required="required" placeholder="Digite seu Nome" />
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12 form-group">
+                    <input id="email" type="email" class="email form-control" name="email" required="required" placeholder="Digite seu E-mail" />
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <input id="telefone" type="text" class="telefone form-control" name="telefone" placeholder="Digite seu Telefone" />
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="checkbox">
+                        <label style="padding-left: 0px;">
+                            Você tem interesse:
+                        </label>
+                    </div>
+                    <div class="checkbox">
+                        <label>
+                            <input type="checkbox" name="checkboxResidenciais" class="checkboxResidenciais" value="residenciais"> Lotes residenciais
+                        </label>
+                    </div>
+                    <div class="checkbox">
+                        <label>
+                            <input type="checkbox" name="checkboxComerciais" class="checkboxComerciais" value="comerciais"> Lotes comerciais
+                        </label>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <textarea id="form-mensagem" class="msg form-control" rows="3" name="mss" required="required" placeholder="Mensagem..."></textarea>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="checkbox">
+                        <label>
+                            <input type="checkbox" name="checkboxAfrincorp" class="checkboxAfrincorp" value="afrincorp"> Aceito receber informações da Afrincorp
+                        </label>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <button type="submit" class="btn_enviar btn" title="enviar" name="enviar_email" value="enviar"></button>
+            </div>
+        </div>
+    </form>
+</div> -->
+<div id="produto" class="container-fluid padding-off produto">
+    <div class="container tt-1">
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 padding-off text-center">
+                <p>
+                    PROJECTO URBANÍSTICO DE UM BAIRRO PLANEADO COM UMA DAS MAIORES<br />
+                    LAGOAS ARTIFICIAIS DE ÁGUAS CRISTALINAS DO MUNDO,<br />
+                    BY <span>CRYSTAL LAGOONS</span><sup>®</sup>.
+                </p>
+            </div>
+        </div>
+    </div>
     <div class="container">
         <div class="row">
-            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-10 col-lg-offset-1">
-                <div class="col-xs-12 col-sm-1 col-md-1 col-lg-1 padding-off"></div>
-                <div class="col-xs-12 col-sm-5 col-md-5 col-lg-5 padding-off">
-                    <form method="post" role="form" action="<?php echo base_url("contato") ?>">
-                        <div class="group-form">
-                            <div class="col-xs-12 col-sm-12 col-md-12 padding-off contato">
-                                <div class="col-xs-12 col-sm-12 col-md-12 form-group">
-                                    <h5>Quer receber mais informações?</h5>
-                                </div>
-                                <div class="col-xs-12 col-sm-12 col-md-12 form-group">
-                                    <input id="nome" type="text" class="nome form-control" name="nome" required="required" placeholder="Digite seu Nome" />
-                                </div>
-                                <div class="col-xs-12 col-sm-12 col-md-12 form-group">
-                                    <input id="email" type="email" class="email form-control" name="email" required="required" placeholder="Digite seu E-mail" />
-                                </div>
-                                <div class="col-xs-12 col-sm-12 col-md-12">
-                                    <input id="telefone" type="text" class="telefone form-control" name="telefone" placeholder="Digite seu Telefone" />
-                                </div>
-                                <div class="col-xs-12 col-sm-12 col-md-12">
-                                    <div class="checkbox">
-                                        <label style="padding-left: 0px;">
-                                            Você tem interesse:
-                                        </label>
-                                    </div>
-                                    <div class="checkbox">
-                                        <label>
-                                            <input type="checkbox" name="checkboxResidenciais" class="checkboxResidenciais" value="residenciais"> Lotes residenciais
-                                        </label>
-                                    </div>
-                                    <div class="checkbox">
-                                        <label>
-                                            <input type="checkbox" name="checkboxComerciais" class="checkboxComerciais" value="comerciais"> Lotes comerciais
-                                        </label>
-                                    </div>
-                                </div>
-                                <div class="col-xs-12 col-sm-12 col-md-12">
-                                    <textarea id="form-mensagem" class="msg form-control" rows="3" name="mss" required="required" placeholder="Mensagem..."></textarea>
-                                </div>
-                                <div class="col-xs-12 col-sm-12 col-md-12">
-                                    <div class="checkbox">
-                                        <label>
-                                            <input type="checkbox" name="checkboxAfrincorp" class="checkboxAfrincorp" value="afrincorp"> Aceito receber informações da Afrincorp
-                                        </label>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-xs-12 col-sm-12 col-md-12">
-                                <button type="submit" class="btn_enviar btn" title="enviar" name="enviar_email" value="enviar"></button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-                <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 padding-off homem hidden-xs">
-                    <img class="img-responsive" src="<?= base_url(); ?>assets/images/homem.png" alt="Novo Luanda Leste">
-                </div>
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 text-center">
+                <img class="img-responsive center-block hidden-xs" src="<?= base_url(); ?>assets/images/dt.png" alt="Novo Luanda Leste">
+            </div>
+        </div>
+    </div>
+    <div class="container tt-2">
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-10 col-lg-offset-1 text-center box">
+                <p>
+                    O NOVO LUANDA LESTE FOI CONCEBIDO PARA SER UM BAIRRO PLANEADO COM <span>10 MILHOES DE M²</span>,<br />
+                    EM SEU ENTORNO SERÁ ENTREGUE UM COMPLEXO INTERNACIONAL DE LAZER COM <span>1,5 HECTARES</span><br />
+                    DESTINADOS A UMA PRAIA ARTIFICIAL BY <span>CRYSTAL LAGOONS</span><sup>®</sup>.
+                </p>
+                <p>
+                    UM PROJECTO HISTÓRICO QUE VAI <span>REVOLUCIONAR O CRESCIMENTO EM ANGOLA</span><br />
+                    PROPORCIONANDO MAIS COMODIDADE, BEM-ESTAR, ACESSIBILIDADE E QUALIDADE DE VIDA.
+                </p>
             </div>
         </div>
     </div>
 </div>
+<div class="video">
+    <div class="box-video">
+        <div class="embed-responsive embed-responsive-16by9">
+            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/N1kTP7dai4M"></iframe>
+        </div>
+    </div>
+</div>
+
 
 
 
